f3b7c570c4196e209c85806436baefa6661c41c0
samue <samue@DESKTOP-MRTRBVE>
2021-10-17 14:15:04 -0400

    Add project files.

---
+++ Numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <cstdlib>
+
+int main() {
+	int e = 1;
+	int o = 1;
+	long long n = 0;
+	if (o == 1)
+		do {
+
+			std::cout << n << "\n";
+			n = n + 1;
+
+		} while (e == 1);
+}
